e28c8d5c5556c7138698f6507e7213fa029b7f12
David GILLARD <david.gillard@ac-lille.fr>
2022-02-05 13:39:08 +0100

    add information file

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -1,9 +1,14 @@
+// fichier : hello_world.go
+// auteur : David GILLARD
+// date : 05/02/2022
+
 package greeting
+
 import "fmt"
 
 // HelloWorld greets the world.
 func HelloWorld() string {
-    message := "Hello, World!"
-    fmt.Println(message)
-    return message
+	message := "Hello, World!"
+	fmt.Println(message)
+	return message
 }
