252be6ca68597998182398c69475963adee1a0fb
David GILLARD <david.gillard@ac-lille.fr>
2022-02-06 15:24:28 +0100

    add solution for welcome_to_tech_palace

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,27 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
+	// c := strings.ToUpper(customer)
+	// return fmt.Sprintf("Welcome to the Tech Palace, %s", c)
+
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	star := strings.Repeat("*", numStarsPerLine)             // method repeat of funct strings
+	return fmt.Sprintf("%s\n%s\n%s", star, welcomeMsg, star) // customize output
+
 }
 
 // CleanupMessage cleans up an old marketing message.
+// Fisrt we replace all occurance of star by empty string but we must delete space too and it's possible with TrimSpace
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
 }
